0167aeba2f3baa0bfab3cc634e95888ce9d434b3
Stewart Schuler <Stewart.Schuler@gmail.com>
2024-05-21 17:44:02 -0400

    Added automatic ID3v2 tag population for downloaded mp3s

--- script.sh
+++ script.sh
@@ -1,9 +1,15 @@
 DL_LOCATION="podcasts/"
 TMP_LOCATION="tmp_web_dl/"
 
+ID3_ARTIST="Dr. Jordan Cooper"
+ID3_ALBUM="Just and Sinner Podcast"
+ID3_TITLE="" # To be derived from filename
+
+
 mkdir -p $DL_LOCATION
 mkdir -p $TMP_LOCATION
 
+ID3_TRACK_NUM=1
 while IFS= read -r POD_PAGE
 do
   wget $POD_PAGE -O $TMP_LOCATION/webpage
@@ -23,8 +29,15 @@ do
   FILENAME=$(echo $DL_LINK | grep -Eo "[^/]*mp3") 
   wget $DL_LINK -O $DL_LOCATION/$FILENAME
 
+
+  # Edit ID3v2 mp3 tags
+  ID3_TITLE=$DATE_STAMP"_"$SHOW_TITLE
+  id3v2 -a "$ID3_ARTIST" -A "$ID3_ALBUM" -t "$ID3_TITLE" -T $ID3_TRACK_NUM -2 $DL_LOCATION/$FILENAME
+
   # Prefix filename with sortable date string
   mv $DL_LOCATION/$FILENAME $DL_LOCATION/$DATE_STAMP"_"$SHOW_TITLE".mp3"
+	
+  ID3_TRACK_NUM=$((ID3_TRACK_NUM+1))
 
 done < "$1"
 rm -rf $TMP_LOCATION
